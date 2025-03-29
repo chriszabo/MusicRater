015c5170baf1bd23550f7b1e50804f526bd512ae
cs <christian.szabo@botanicly.de>
2025-03-29 04:08:42 +0100

    add search albums spotify

--- spotify.js
+++ spotify.js
@@ -129,3 +129,18 @@ export const getArtistTopTracks = async (artistId) => {
   const data = await response.json();
   return data.tracks;
 };
+
+export const searchAlbums = async (query) => {
+  const token = await getAccessToken();
+  const response = await fetch(
+    `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=album&limit=1`,
+    {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+  );
+  const data = await response.json();
+  return data.albums?.items[0];
+};
+
